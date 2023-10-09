fa6448b377590deb303a4102e88ae495e02ce08f
여름 <pjw7200@ajou.ac.kr>
2023-10-09 16:18:30 +0900

    Initial commit

---
+++ src/test/java/hellojpa/hellojpa/HellojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package hellojpa.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/hellojpa/hellojpa/JpaConnectionTest.java
@@ -0,0 +1,29 @@
+package hellojpa.hellojpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class JpaConnectionTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void test(){
+        Member member = new Member();
+        member.setId(1L); member.setName("userA");
+
+        em.persist(member);
+
+        Member findMember = em.find(Member.class, 1L);
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo("userA");
+    }
+}
