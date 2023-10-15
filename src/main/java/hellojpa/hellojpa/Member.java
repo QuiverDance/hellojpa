4751f014fb6fa0dfdfcf194653431eb7f8bb56b7
여름 <pjw7200@ajou.ac.kr>
2023-10-15 16:41:36 +0900

    ADD: Key & Column mapping

--- src/main/java/hellojpa/hellojpa/Member.java
+++ src/main/java/hellojpa/hellojpa/Member.java
@@ -3,14 +3,28 @@ package hellojpa.hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
+
+    @Column(name = "name", insertable = true, updatable = true, nullable = false)
     private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    private LocalDate createdDate;
+    private LocalDate lastModifiedDate;
+
+    @Lob
+    private String description;
 }
---
+++ src/main/java/hellojpa/hellojpa/RoleType.java
@@ -0,0 +1,5 @@
+package hellojpa.hellojpa;
+
+public enum RoleType {
+    ADMIN, GUEST
+}
