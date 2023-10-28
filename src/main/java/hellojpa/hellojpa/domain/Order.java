f75a275a4a52250753822190528975e8f247b620
여름 <pjw7200@ajou.ac.kr>
2023-10-28 16:54:49 +0900

    ADD: add lazy loading and cascade

--- src/main/java/hellojpa/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/hellojpa/domain/Order.java
@@ -20,7 +20,7 @@ public class Order {
     private Long id;
 //    @Column(name = "MEMBER_ID")
 //    private Long memberId;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
     private LocalDateTime orderDate;
@@ -28,14 +28,14 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
     /*
     아래 매핑은 굳이 필요 없음, OrderItem에 OrderId가 있으므로 orderItems를 만들어 낼 수 있음.
      */
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     public void addOrderItem(OrderItem orderItem){
